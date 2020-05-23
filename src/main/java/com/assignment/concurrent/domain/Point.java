7022a5f5040d20fc3bf32132f4003f6bc80267bf
Clement Saw Kiat Chun <cskiat06@gmail.com>
2020-05-23 20:01:33 +0800

    Update Point.java

--- src/main/java/com/assignment/concurrent/domain/Point.java
+++ src/main/java/com/assignment/concurrent/domain/Point.java
@@ -1,13 +1,24 @@
 package com.assignment.concurrent.domain;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Point {
     private double x;
     private double y;
     private boolean hasEdge = false;
+    private ReentrantLock lock = new ReentrantLock();
     
     public Point(double x, double y){
         this.x = x;
         this.y = y;
+    }
+    
+    public double getX(){
+        return x;
+    }
+    
+    public double getY(){
+        return y;
     }
     
     public boolean getHasEdge(){
@@ -18,7 +29,18 @@ public class Point {
         this.hasEdge = true;
     }
     
+    public boolean isLocked(){
+        return lock.isLocked();
+    }
+    
+    public void unlock(){
+        if(lock.isHeldByCurrentThread()){
+            lock.unlock();
+        }
+    }
+    
+    @Override
     public String toString(){
-        return ("x: " + this.x + ". y: " + this.y + ". Edge formed: " + this.hasEdge);
+        return ("x: " + x + ", y: " + y);
     }
 }
