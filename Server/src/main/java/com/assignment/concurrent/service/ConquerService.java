7ed3cdf049202d0073105553a882a31d82d034a3
1998chun <34387770+1998chun@users.noreply.github.com>
2020-06-23 16:14:34 +0800

    points service should be a service component

--- Server/src/main/java/com/assignment/concurrent/service/ConquerService.java
+++ Server/src/main/java/com/assignment/concurrent/service/ConquerService.java
@@ -4,23 +4,21 @@ import com.assignment.concurrent.domain.Board;
 import com.assignment.concurrent.domain.Point;
 import com.assignment.concurrent.domain.PointPairingTask;
 import com.assignment.concurrent.domain.UserInputMessage;
-import com.assignment.concurrent.util.RunnableFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 import java.util.Timer;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Service
 public class ConquerService {
 
-    private final RunnableFactory runnableFactory;
+    private final PointsService pointsService;
     private final MessageService messageService;
 
-    public ConquerService(RunnableFactory runnableFactory, PointsService pointsFactory) {
-        this.runnableFactory = runnableFactory;
+    public ConquerService(PointsService pointsService, MessageService messageService) {
+        this.pointsService = pointsService;
         this.messageService = messageService;
     }
 
@@ -31,22 +29,17 @@ public class ConquerService {
         Timer timer = new Timer();
         ExecutorService executorService = Executors.newFixedThreadPool(t);
         StopTaskService stopTask = new StopTaskService(executorService, timer);
-        Set<Point> points = PointsService.generatePoints(n);
+        Set<Point> points = pointsService.generatePoints(n);
         Point[] pointsArr = new Point[n];
         points.toArray(pointsArr);
         PointPairingTask[] pointPairingTasks = new PointPairingTask[t];
         for (int i = 0; i < t; i++) {
-            pointPairingTasks[i] = new PointPairingTask(pointsArr);
+            pointPairingTasks[i] = new PointPairingTask(pointsArr, messageService);
         }
         Board board = new Board(pointsArr, pointPairingTasks, timer, executorService
                 , stopTask, m);
-        Callable C = new PointPairingTask(pointsArr, messageService);
-        // board.setPoints(points);
-        //can change to Thread pool
-//        Thread thread1 = new Thread(runnableFactory.createRunnableSpammer(points));
-//        Thread thread2 = new Thread(runnableFactory.createRunnableSpammer(points));
-//        thread1.start();
-//        thread2.start();
+//        Callable C = new PointPairingTask(pointsArr, messageService);
+        board.startProgram();
         return board;
     }
 }
